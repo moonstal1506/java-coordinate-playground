0c0105e12b52a9e869509b1763cbc724f85c34fe
moonstal1506 <moonstal1506@gmail.com>
2022-04-28 22:00:32 +0900

    feat: 삼각형 면적구하기(일단구현)

--- src/main/java/coordinate/Main.java
+++ src/main/java/coordinate/Main.java
@@ -28,24 +28,40 @@ public class Main {
             Point a = points.get(0);
             Point b = points.get(1);
 
-            double distance = Math.sqrt(Math.pow(a.getX() - b.getX(), 2) +
-                    (Math.pow(a.getY() - b.getY(), 2)));
-
-            System.out.println("두점사이의 거리는" + distance);
+            System.out.println("두점사이의 거리는" + calculatePointsDistance(a, b));
         }
 
+        if (points.size() == 3) {
+            Point a = points.get(0);
+            Point b = points.get(1);
+            Point c = points.get(2);
+
+            double distanceAB = calculatePointsDistance(a, b);
+            double distanceBC = calculatePointsDistance(b, c);
+            double distanceAC = calculatePointsDistance(a, c);
+
+            double s = (distanceAB + distanceBC + distanceAC) / 2;
+            double area = Math.sqrt(s * (s - distanceAB) * (s - distanceBC) * (s - distanceAC));
+
+            System.out.println("삼각형 넓이는 " + area);
+        }
 
         if (points.size() == 4) {
-            Set<Integer> xSet = getSet(points,Point::getX);
-            Set<Integer> ySet = getSet(points,Point::getY);
+            Set<Integer> xSet = getSet(points, Point::getX);
+            Set<Integer> ySet = getSet(points, Point::getY);
 
-            if(!(xSet.size()==2&&ySet.size()==2)){
+            if (!(xSet.size() == 2 && ySet.size() == 2)) {
                 throw new IllegalArgumentException("직사각형 아님");
             }
 
-            int areaSize=calculateDistance(xSet)*calculateDistance(ySet);
-            System.out.println("사각형 넓이는 " + areaSize);
+            int area = calculatePointDistance(xSet) * calculatePointDistance(ySet);
+            System.out.println("사각형 넓이는 " + area);
         }
+    }
+
+    private static double calculatePointsDistance(Point point1, Point point2) {
+        return Math.sqrt(Math.pow(point1.getX() - point2.getX(), 2) +
+                (Math.pow(point1.getY() - point2.getY(), 2)));
     }
 
     private static Set<Integer> getSet(List<Point> points, Function<Point, Integer> function) {
@@ -54,8 +70,8 @@ public class Main {
                 .collect(toSet());
     }
 
-    private static int calculateDistance(Set<Integer> set){
+    private static int calculatePointDistance(Set<Integer> set) {
         List<Integer> list = new ArrayList<>(set);
-        return Math.abs(list.get(0)-list.get(1));
+        return Math.abs(list.get(0) - list.get(1));
     }
 }
