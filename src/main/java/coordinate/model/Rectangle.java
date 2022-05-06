6d6ecbe6bdcbbdf5c7acbc1657b30c9633063d0d
moonstal1506 <moonstal1506@gmail.com>
2022-05-06 23:43:29 +0900

    feat: 직사각형 예외

---
+++ src/main/java/coordinate/model/Rectangle.java
@@ -0,0 +1,45 @@
+package coordinate.model;
+
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toSet;
+
+public class Rectangle extends Figure{
+
+    private static final int SET_SIZE = 2;
+
+    public Rectangle(List<Point> points) {
+        checkRectangle(points);
+        super.points = points;
+    }
+
+    private void checkRectangle(List<Point> points){
+        Set<Integer> xSet = xyToSet(points,Point::getX);
+        Set<Integer> ySet = xyToSet(points,Point::getY);
+
+        if(!(isRectangle(xSet) && isRectangle(ySet))){
+            throw new IllegalArgumentException("사각형이 아닙니다.");
+        }
+    }
+
+    private Set<Integer> xyToSet(List<Point> points, Function<Point,Integer> function) {
+        return points.stream().map(function).collect(toSet());
+    }
+
+    private boolean isRectangle(Set<Integer> xSet) {
+        return xSet.size() == SET_SIZE;
+    }
+
+    @Override
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public String reportArea() {
+        return null;
+    }
+}
---
+++ src/test/java/coordinate/model/RectangleTest.java
@@ -0,0 +1,31 @@
+package coordinate.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RectangleTest {
+
+    private List<Point> points;
+
+    @BeforeEach
+    void setUp() {
+        points = new ArrayList<>();
+    }
+
+    @Test
+    void 직사각형_예외() {
+        points.add(new Point(11,10));
+        points.add(new Point(22,10));
+        points.add(new Point(21,13));
+        points.add(new Point(10,18));
+        assertThrows(IllegalArgumentException.class, ()->{
+            new Rectangle(points);
+        });
+    }
+}
